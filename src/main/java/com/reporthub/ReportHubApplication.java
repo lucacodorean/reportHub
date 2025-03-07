f2cfd9d33e76c432d1dc6c3e8a19e948c0d8f8e4
Luca Codorean <codoreanluca@gmail.com>
2025-03-07 12:37:13 +0200

    feat: File structure.

---
+++ src/main/java/com/reporthub/ReportHubApplication.java
@@ -0,0 +1,13 @@
+package com.reporthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reporthub/ReportubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reporthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
