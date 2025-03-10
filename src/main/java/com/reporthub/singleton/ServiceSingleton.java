a199f26148d63ec8e7e05ac26941baacb4e823ec
Daniel <deaconescu.daniel11@gmail.com>
2025-03-10 18:49:56 +0200

    feat: Implemented Report getAll

--- src/main/java/com/reporthub/singleton/ServiceSingleton.java
+++ src/main/java/com/reporthub/singleton/ServiceSingleton.java
@@ -1,10 +1,8 @@
 package com.reporthub.singleton;
 
 import com.reporthub.service.IReportService;
-import com.reporthub.service.IAuthorizationService;
 import com.reporthub.service.IUserService;
 import com.reporthub.service.implementation.IReportServiceImpl;
-import com.reporthub.service.implementation.IAuthorizationServiceImpl;
 import com.reporthub.service.implementation.IUserServiceImpl;
 import lombok.Getter;
 
@@ -18,12 +16,9 @@ public class ServiceSingleton {
     @Getter
     private static IReportService reportService;
 
-    @Getter
-    private static IAuthorizationService authorizationService;
-
     static {
         userService = new IUserServiceImpl();
         reportService = new IReportServiceImpl();
-        authorizationService = new IAuthorizationServiceImpl();
     }
+
 }
